3183f92c58196c9f62502bfd305481777934815a
Melih <mlhdzdr@gmail.com>
2021-12-30 22:49:11 +0300

    fixed fullscreen problem in Gallery.js

--- frontend/src/screens/GalleryScreen.js
+++ frontend/src/screens/GalleryScreen.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 import "../styles/Gallery/gallery.css";
 import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
 export default function GalleryScreen() {
     const photos = [
@@ -40,13 +41,37 @@ export default function GalleryScreen() {
             height: 4
         },
     ];
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
     return (
         <div className="gallery">
             <div className="headerStage">
                 <div className="headerText">Galeri</div>
             </div>
             <div className="photoswipeGallery">
-                <Gallery photos={photos} direction={"column"}/>
+                <Gallery photos={photos} direction={"column"} onClick={openLightbox}/>
+                <ModalGateway>
+                    {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                        currentIndex={currentImage}
+                        views={photos.map(x => ({
+                            ...x,
+                            srcset: x.srcSet,
+                            caption: x.title
+                        }))}
+                        />
+                    </Modal>
+                    ) : null}
+                </ModalGateway>
             </div>
         </div>
     )
